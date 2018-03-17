0f8bb9ded735d24642da808e74c80e7431ae16c7
CharizadFire58 <30486280+CharizadFire58@users.noreply.github.com>
2018-03-17 19:13:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const pokemon1 = require('./randomPokemon')()
-const pokemon2 = require('./randomPokemon')()
 const berrie1 = require('./randomBerries')()
 const berrie2 = require('./randomBerries')()
-
-console.log('pokemon1', pokemon1)
-console.log('pokemon2', pokemon2)
 
 console.log('berrie1', berrie1)
 console.log('berrie2', berrie2)
