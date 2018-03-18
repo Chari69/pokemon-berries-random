a0435f284ac7aeee049bb2deb8e9718a4c24ad38
CharizadFire58 <30486280+CharizadFire58@users.noreply.github.com>
2018-03-18 00:17:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const berrie1 = require('./randomBerries')();
-const berrie2 = require('./randomBerries')();
+const berrie1 = require('./randomBerries')()
+const berrie2 = require('./randomBerries')()
 
-console.log('berrie1', berrie1);
-console.log('berrie2', berrie2);
+console.log('berrie1', berrie1)
+console.log('berrie2', berrie2)
